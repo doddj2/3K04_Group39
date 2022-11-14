262d0bcddb22d0aa21c6aab80e3a1b98ce2546d4
zareen <kabirz@mcmaster.ca>
2022-11-14 16:20:26 -0500

    Added rate adaptive inputs and started adaptive block

---
+++ simulink_group39/assignment_1_ert_rtw/assignment_1_data.c
@@ -0,0 +1,149 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: assignment_1_data.c
+ *
+ * Code generated for Simulink model 'assignment_1'.
+ *
+ * Model version                  : 1.37
+ * Simulink Coder version         : 9.7 (R2022a) 13-Nov-2021
+ * C/C++ source code generated on : Mon Nov 14 16:18:54 2022
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "assignment_1.h"
+
+/* Block parameters (default storage) */
+P_assignment_1_T assignment_1_P = {
+  /* Expression: 1
+   * Referenced by: '<S5>/Constant'
+   */
+  1.0,
+
+  /* Expression: 0
+   * Referenced by: '<S5>/Constant1'
+   */
+  0.0,
+
+  /* Expression: -1
+   * Referenced by: '<S3>/FXOS8700 6-Axes Sensor'
+   */
+  -1.0,
+
+  /* Expression: SampleTime
+   * Referenced by: '<S4>/ATR_CMP_DETECT D0'
+   */
+  -1.0,
+
+  /* Expression: SampleTime
+   * Referenced by: '<S4>/VENT_CMP_DETECT D1'
+   */
+  -1.0,
+
+  /* Expression: 120
+   * Referenced by: '<S1>/ppm'
+   */
+  120.0,
+
+  /* Expression: 60000
+   * Referenced by: '<S9>/1min = 60sec = 60000msec2'
+   */
+  60000.0,
+
+  /* Expression: 130
+   * Referenced by: '<S1>/ARP msec'
+   */
+  130.0,
+
+  /* Expression: 5
+   * Referenced by: '<S1>/Atrial Pluse Width msec'
+   */
+  5.0,
+
+  /* Expression: 3
+   * Referenced by: '<S1>/mode'
+   */
+  3.0,
+
+  /* Expression: 120
+   * Referenced by: '<S1>/VRP msec'
+   */
+  120.0,
+
+  /* Expression: 5
+   * Referenced by: '<S1>/Ventricular Pluse Width msec'
+   */
+  5.0,
+
+  /* Expression: 3.2
+   * Referenced by: '<S1>/Atrial Amplitude(volt)'
+   */
+  3.2,
+
+  /* Expression: 5
+   * Referenced by: '<S8>/Constant'
+   */
+  5.0,
+
+  /* Expression: 100
+   * Referenced by: '<S8>/Gain'
+   */
+  100.0,
+
+  /* Expression: 3.5
+   * Referenced by: '<S1>/Ventricular Amplitude'
+   */
+  3.5,
+
+  /* Expression: 5
+   * Referenced by: '<S8>/Constant1'
+   */
+  5.0,
+
+  /* Expression: 100
+   * Referenced by: '<S8>/Gain1'
+   */
+  100.0,
+
+  /* Expression: 2
+   * Referenced by: '<S5>/Switch'
+   */
+  2.0,
+
+  /* Expression: 2.3
+   * Referenced by: '<S1>/Atrial threshold(volt)'
+   */
+  2.3,
+
+  /* Expression: 3.3
+   * Referenced by: '<S1>/Voltage'
+   */
+  3.3,
+
+  /* Expression: 100
+   * Referenced by: '<S7>/Gain'
+   */
+  100.0,
+
+  /* Expression: 2.3
+   * Referenced by: '<S1>/Ventricle threshold(volt)'
+   */
+  2.3,
+
+  /* Expression: 100
+   * Referenced by: '<S7>/Gain1'
+   */
+  100.0
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
